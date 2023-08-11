977360b23d5f138e2543acaccb5f710de50b0f58
Eugenia Clarissa Anjani <e.clarissa.anjani@tum.de>
2023-08-11 19:09:54 +0100

    working local scripts integrated

---
+++ calculate_value.py
@@ -0,0 +1,78 @@
+def calculate_objective_function(patient_id, confirmed_schedule, weight, leftover, prescribed_total_dialysis_hours, num_required_dialysis_sessions, num_patients, max_allowed_deviation): 
+    # print(patient_id)
+    # print(confirmed_schedule)
+    # print("Weight is ", weight, " and the penalty is ", count_penalty(leftover), "the prescribed dialysis hours is ", prescribed_total_dialysis_hours)
+    # print("The number of confirmed time slots is ", len(confirmed_schedule), " and the total confirmed hours is ", count_total_confirmed_hours(confirmed_schedule))
+    first_term_result = get_first_term(weight, leftover, num_required_dialysis_sessions, num_patients, max_allowed_deviation)
+    second_term_result = get_second_term(weight, prescribed_total_dialysis_hours, confirmed_schedule, prescribed_total_dialysis_hours)
+    
+    num_confirmed_timeslots = len(confirmed_schedule)
+    obj_function_inc = first_term_result + second_term_result
+    count_one = 0
+    count_two = 0
+    count_three = 0
+    count_zero = 0
+    
+    if len(confirmed_schedule) == 1:
+        count_one = 1
+    elif len(confirmed_schedule) == 2:
+        count_two = 1
+    elif len(confirmed_schedule) == 3:
+        count_three = 1
+    elif len(confirmed_schedule) == 0:
+        count_zero = 1
+        
+    # print("this is from  calculate_obj_function", obj_function_inc, num_confirmed_timeslots, count_one, count_two, count_three, count_zero)
+        
+    return obj_function_inc, num_confirmed_timeslots, count_one, count_two, count_three, count_zero
+
+def get_first_term(weight, leftover, num_required_dialysis_sessions, num_patients, max_allowed_deviation):
+    return weight * count_penalty(leftover, num_required_dialysis_sessions, num_patients, max_allowed_deviation)
+
+def get_second_term(weight, prescribed_total_diaylsis_hours, confirmed_schedule, prescribed_total_dialysis_hours):
+    hour_diff = prescribed_total_dialysis_hours - count_total_confirmed_hours(confirmed_schedule)
+    
+    if hour_diff < 0: 
+        multiplier = 0
+    else:
+        multiplier = hour_diff
+    
+    return weight * multiplier
+
+
+
+# TODO CHANGE THE DOCSTRING
+def count_leftover_appointments(num_allocated_appointments, num_required_dialysis_sessions):
+    """
+    The function is to create patient profiles for each of the patients
+
+    Args:
+        patient_number (int): The length of the rectangle
+
+    Returns:
+        patient_profile_dict (dict): Return a dictionary of patient profiles with the patient number, 
+        ideal schedule, priority and weight
+    """
+    # beta_j is the total number of leftover appointmnets
+    return num_required_dialysis_sessions - num_allocated_appointments
+
+
+# count penalty
+def count_penalty(num_leftover_appointments, num_required_dialysis_sessions, num_patients, max_allowed_deviation):
+    # the number of leftover appointmnets are 0, then there is no penalty
+    if num_leftover_appointments == 0:
+        penalty = 0
+    else:
+        penalty = (num_required_dialysis_sessions * num_patients * max_allowed_deviation) ** num_leftover_appointments
+    
+    return penalty
+
+
+def count_total_confirmed_hours(patient_sched):
+    total_hours = 0
+
+    for schedule in patient_sched:
+        schedule_hours = len(schedule['schedule'])
+        total_hours += schedule_hours
+
+    return total_hours
---
+++ schedule_appointment.py
@@ -0,0 +1,323 @@
+   
+import random
+
+# Example for testing
+# bed_id = "b1"
+# patient_day = 1
+# patient_schedule = {1: {1600:0,1700:0,1800:0}}
+# patient_id = "p1"
+def change_bed_occupancy(bed_id, patient_schedule, patient_day, patient_id, bed_objects_list):
+    # change the bed occupancy from available (meaning 0) to the patient id
+    for bed in bed_objects_list:
+        # print("this is the bed_id whose bed occupancy i will change to the patient id ", bed.bed_id)
+        if bed.bed_id == bed_id:
+            # print("yes")
+            # change the occupancy based on the possible_sched
+            # print("this is the patient_day whose bed occupancy i will change to the patient ID", patient_id, patient_day)
+            for time_p in patient_schedule:
+                # print(bed_schedule[patient_day][time_p])
+                # change from 0 to the patient_id
+                # print("This is the time that I will change to 0 ", bed.bed_schedule[patient_day][time_p])
+                bed.bed_schedule[patient_day][time_p] = patient_id
+
+def free_up_bed_occupancy(bed_id, patient_schedule, patient_day, patient_id, bed_objects_list):
+    # change the bed occupancy from available (meaning 0) to the patient id
+    for bed in bed_objects_list:
+        # print("this is the bed_id whose bed occupancy i will free up", bed.bed_id)
+        # print("this is the patient_schedule whose bed occupancy i will free up ", patient_schedule)
+        if bed.bed_id == bed_id:
+            # print("yes")
+            # change the occupancy based on the possible_sched
+            # print("this is the patient_day ", patient_day)
+            for time_p in patient_schedule:
+                # print("This is the time that I will change to 0 ", time_p)
+                # change from 0 to the patient_id
+                bed.bed_schedule[patient_day][time_p] = 0
+
+# requested_time_slot = {1600:0,1700:0,1800:0}}
+# requested_day = 1
+# requested_time = 1600
+
+def check_bed_availability(bed_id, requested_time_slot, requested_day, bed_objects_list):
+    availablility_counter = 0
+    for bed in bed_objects_list:
+        if bed.bed_id == bed_id:
+            # check if its free
+            for requested_time in requested_time_slot:
+                if bed.bed_schedule[requested_day][requested_time] == 0: 
+                    # print("requested day ", requested_day, " requested time ", requested_time)
+                    availablility_counter += 1
+
+            if availablility_counter == len(requested_time_slot):
+                return True
+            else:
+                return False
+
+def reserve_patient_beds(all_possible_patient_schedules, patient_schedules_in_list, patient_schedules_out_list, bed_objects_list):
+    # Example for testing
+    # bed_id = "b1"
+    # patient_day = 1
+    # patient_schedule = {1: {1600:0,1700:0,1800:0}}
+    # patient_id = "p1"
+    #change_bed_occupancy(bed_id, single_patient_day_schedule, patient_day, patient_id)
+
+    ## Look through the first matched availability
+    for patient_sched in all_possible_patient_schedules:
+        patient_schedules_out_dict = {}
+        patient_schedules_in_dict = {}
+        days = list(patient_sched['possible_allocated_schedule'].keys())
+        # print("this is the length ", len(patient_sched['possible_allocated_schedule']))
+        for requested_day in days:
+            # print("this is the requested day ", requested_day)
+            # print("this is patient_sched['possible_allocated_schedule'][requested_day]", patient_sched['possible_allocated_schedule'][requested_day])
+            requested_sched = patient_sched['possible_allocated_schedule'][requested_day]
+            # print(patient_sched['possible_allocated_schedule'])
+            bed_id = patient_sched['bed']
+            #check_bed_availability(bed_id, requested_time_slot, requested_day)
+            # print("first if statement ", bed_id, requested_sched, " day ", requested_day, " for patient ", patient_sched['patient'])
+            if check_bed_availability(bed_id, requested_sched, requested_day, bed_objects_list) == True:
+                # print("you passed the bed availablity test") 
+
+                ## Add the patient's schedule to the patient_schedules_in_dict and list
+                patient_schedules_in_dict = {
+                    'patient': patient_sched['patient'],
+                    'bed': patient_sched['bed'],
+                    'day': requested_day,
+                    'schedule': patient_sched['possible_allocated_schedule'][requested_day]
+                }
+                patient_schedules_in_list.append(patient_schedules_in_dict)
+
+                #print(requested_sched)
+                change_bed_occupancy(bed_id, requested_sched, requested_day, patient_sched['patient'], bed_objects_list)
+
+                ## Remove the patient schedule from patient_schedules_out_list
+                if patient_schedules_in_dict in patient_schedules_out_list:
+                    patient_schedules_out_list.remove(patient_schedules_in_dict)
+
+            else:
+                # bed turns out is not available so add the patient's schedule to the patient_schedules_out_dict and list
+                patient_schedules_out_dict = {
+                    'patient': patient_sched['patient'],
+                    'bed': patient_sched['bed'],
+                    'day': requested_day,
+                    'schedule': patient_sched['possible_allocated_schedule'][requested_day]
+                }
+                # if the schedule is not already in the out list, then add it to the out list
+                if patient_schedules_out_dict not in patient_schedules_out_list:
+                    patient_schedules_out_list.append(patient_schedules_out_dict)
+                    # print("Unfortunately this ", patient_schedules_out_dict, " could not be added the bed is not available on", requested_day)
+
+                # make sure to remove it from the patient_schedules_in_list
+                if patient_schedules_out_dict in patient_schedules_in_list:
+                    patient_schedules_in_list.remove(patient_schedules_out_dict)
+
+                # make sure to free up the bed occupancy because it was previously in the in_list
+                free_up_bed_occupancy(bed_id, requested_sched, requested_day, patient_sched['patient'], bed_objects_list)
+                
+    return patient_schedules_out_list, patient_schedules_in_list
+
+
+def check_time_spacing(patient_schedules_out_list, patient_schedules_in_list, bed_objects_list):
+    assigned_days = {}
+
+    patient_schedules_in_list_copy = patient_schedules_in_list.copy()
+    
+    for schedule in patient_schedules_in_list_copy:
+        patient = schedule['patient']
+        bed = schedule['bed']
+        day = schedule['day']
+        schedule_data = schedule['schedule']
+
+        # Check if the patient is already assigned a day
+        if patient in assigned_days:
+            # print(assigned_days)
+            # Check if the assigned day is consecutive to the current day
+            if assigned_days[patient] + 1 == day:
+                # Add the patient's information to the patient_schedule_out_list
+                patient_schedule_out_dict = {
+                    'patient': patient,
+                    'bed': bed,
+                    'day': assigned_days[patient] + 1,
+                    'schedule': schedule_data
+                }
+                patient_schedules_out_list.append(patient_schedule_out_dict)
+                # Remove the conflicting appointment from patient_schedules_in_list
+                patient_schedules_in_list.remove(schedule)
+
+                free_up_bed_occupancy(bed, schedule_data, day, patient, bed_objects_list)
+            else:
+                assigned_days[patient] = day
+        else:
+            assigned_days[patient] = day
+            
+    return patient_schedules_out_list, patient_schedules_in_list, assigned_days
+
+def remove_non_consecutive_schedules(patient_schedules_out_list, patient_schedules_in_list):
+    modified_list = []
+
+    for entry in patient_schedules_in_list:
+        schedule = entry['schedule']
+        sorted_times = sorted(schedule.keys())
+
+        # Find consecutive intervals
+        consecutive_intervals = []
+        current_interval = []
+
+        for time in sorted_times:
+            if not current_interval or time - current_interval[-1] == 100:
+                current_interval.append(time)
+            else:
+                consecutive_intervals.append(current_interval)
+                current_interval = [time]
+
+        consecutive_intervals.append(current_interval)
+
+        # Filter out non-consecutive intervals
+        filtered_intervals = [interval for interval in consecutive_intervals if len(interval) > 1]
+
+        # Create modified schedule dictionary
+        modified_schedule = {'patient': entry['patient'], 'bed': entry['bed'], 'day': entry['day'], 'schedule': {}}
+
+        if len(filtered_intervals) > 0:
+            # Append only the first consecutive interval
+            first_interval = filtered_intervals[0]
+            for time in first_interval:
+                modified_schedule['schedule'][time] = schedule[time]
+
+        modified_list.append(modified_schedule)
+
+    return modified_list
+
+
+def remove_occupied_beds(patient_schedules_out_list, patient_schedules_in_list, bed_objects_list, multi_appt_assigned_days):
+    assigned_beds = {}
+    # Create a copy of the patient_schedules_in_list to avoid modifying it during iteration
+    patient_schedules_in_list_copy = patient_schedules_in_list.copy()
+    
+    # shufflel ist
+    random.shuffle(patient_schedules_in_list_copy)
+    
+    for schedule in patient_schedules_in_list_copy:
+        patient_day = str(schedule['patient']) + str(schedule['day'])
+        patient = schedule['patient']
+        bed = schedule['bed']
+        day = schedule['day']
+        schedule_data = schedule['schedule']
+        
+        # Check if the patient is already assigned a day
+        if patient_day in assigned_beds and patient in multi_appt_assigned_days:
+            # Add the patient's information to the patient_schedule_out_list
+            patient_schedules_out_dict = {
+                'patient': patient,
+                'bed': bed,
+                'day': multi_appt_assigned_days[patient] + 1,
+                'schedule': schedule_data
+            }
+            patient_schedules_out_list.append(patient_schedules_out_dict)
+            # Remove the conflicting appointment from patient_schedules_in_list
+            patient_schedules_in_list.remove(schedule)
+
+            free_up_bed_occupancy(bed, schedule_data, day, patient, bed_objects_list)
+        else:
+            assigned_beds[patient_day] = bed
+    
+    return patient_schedules_out_list, patient_schedules_in_list
+
+
+def reorder_list(patient_objects_list):
+    """
+      Reorders the list of patient objects based on the weight value of the patient object. Patient with highest weight comes first
+
+      Args:
+        patient_objects_list: The list of patient objects.
+
+      Returns:
+        A reordered list of patient objects.
+      """
+
+    sorted_list = sorted(patient_objects_list, key=lambda patient: patient.weight, reverse=True)
+    
+    return sorted_list
+
+
+def reserve_patient_beds_by_weight(copy_all_possible_patient_schedules, reordered_patient_object_list, bed_objects_list, patient_schedules_in_list, patient_schedules_out_list):
+    """
+      Uses the reordered patient list (the one by weight) and reserves the patient beds
+      Args:
+        patient_objects_list: The list of patient objects.
+
+      Returns:
+        A reordered list of patient objects.
+      """
+    for patient in reordered_patient_object_list:
+        for patient_sched in copy_all_possible_patient_schedules:
+            # print(patient_sched)
+            # process the patients with the highest weight:
+            if patient.patient_id == patient_sched['patient']:
+                patient_schedules_out_dict = {}
+                patient_schedules_in_dict = {}
+                days = list(patient_sched['possible_allocated_schedule'].keys())
+                # shuffle the days randomly
+                random.shuffle(days)
+                # print("this is the length ", len(patient_sched['possible_allocated_schedule']))
+                for requested_day in days:
+                    # print("this is the requested day ", requested_day)
+                    # print("this is patient_sched['possible_allocated_schedule'][requested_day]", patient_sched['possible_allocated_schedule'][requested_day])
+                    requested_sched = patient_sched['possible_allocated_schedule'][requested_day]
+                    # print(patient_sched['possible_allocated_schedule'])
+                    bed_id = patient_sched['bed']
+                    #check_bed_availability(bed_id, requested_time_slot, requested_day)
+                    # print("first if statement ", bed_id, requested_sched, " day ", requested_day, " for patient ", patient_sched['patient'])
+                    if check_bed_availability(bed_id, requested_sched, requested_day, bed_objects_list) == True:
+                        # print("you passed the bed availablity test") 
+
+                        ## Add the patient's schedule to the patient_schedules_in_dict and list
+                        patient_schedules_in_dict = {
+                            'patient': patient_sched['patient'],
+                            'bed': patient_sched['bed'],
+                            'day': requested_day,
+                            'schedule': patient_sched['possible_allocated_schedule'][requested_day]
+                        }
+                        patient_schedules_in_list.append(patient_schedules_in_dict)
+
+                        #print(requested_sched)
+                        change_bed_occupancy(bed_id, requested_sched, requested_day, patient_sched['patient'], bed_objects_list)
+
+                        ## Remove the patient schedule from patient_schedules_out_list
+                        if patient_schedules_in_dict in patient_schedules_out_list:
+                            patient_schedules_out_list.remove(patient_schedules_in_dict)
+
+                    else:
+                        # bed turns out is not available so add the patient's schedule to the patient_schedules_out_dict and list
+                        patient_schedules_out_dict = {
+                            'patient': patient_sched['patient'],
+                            'bed': patient_sched['bed'],
+                            'day': requested_day,
+                            'schedule': patient_sched['possible_allocated_schedule'][requested_day]
+                        }
+                        # if the schedule is not already in the out list, then add it to the out list
+                        if patient_schedules_out_dict not in patient_schedules_out_list:
+                            patient_schedules_out_list.append(patient_schedules_out_dict)
+                            # print("Unfortunately this ", patient_schedules_out_dict, " could not be added the bed is not available on", requested_day)
+
+                        # make sure to remove it from the patient_schedules_in_list
+                        if patient_schedules_out_dict in patient_schedules_in_list:
+                            patient_schedules_in_list.remove(patient_schedules_out_dict)
+
+                        # make sure to free up the bed occupancy because it was previously in the in_list
+                        free_up_bed_occupancy(bed_id, requested_sched, requested_day, patient_sched['patient'], bed_objects_list)
+                
+    return patient_schedules_out_list, patient_schedules_in_list
+
+
+def set_values_patient(multi_appt_patient_objects_list, multiappt_patient_schedules_in_list):
+    for obj in multi_appt_patient_objects_list:
+        schedule_list = []
+        for patient_sched in multiappt_patient_schedules_in_list:
+            if obj.patient_id == patient_sched['patient']:
+                schedule_entry = {}
+                schedule_entry['bed'] = patient_sched['bed']
+                schedule_entry['day'] = patient_sched['day']
+                schedule_entry['schedule'] = patient_sched['schedule']
+                schedule_list.append(schedule_entry)
+                obj.confirmed_schedule = schedule_list
